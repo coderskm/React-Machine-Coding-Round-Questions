997ded4f58913af439945b27943bdd2b1323ad7d
coderskm <sumitkumarmishra.code@gmail.com>
2026-01-10 00:26:47 +0530

    Stopwatch App

---
+++ react-stopwatch-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Stopwatch from "./Components/Stopwatch";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>REACT STOPWATCH APP</h1>
+      <Stopwatch />
+    </>
+  );
+}
+
+export default App;
